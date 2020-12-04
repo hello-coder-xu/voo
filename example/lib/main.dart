502c4059772be03efd4c86204b4247fad95c42b1
尹旭 <yin_8023@addcn.com>
2020-12-04 16:46:54 +0800

    调整路由

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,53 +3,65 @@ import 'package:flutter/material.dart';
 import 'package:voo/color/index.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'voo Demo',
-    theme: ThemeData(
-      primaryColor: VooColors.primaryColor,
-      accentColor: VooColors.accentColor,
-      scaffoldBackgroundColor: VooColors.pageBgColor,
-      appBarTheme: AppBarTheme(
-        color: Colors.white,
-        elevation: 0,
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'voo Demo',
+      theme: ThemeData(
+        primaryColor: VooColors.primaryColor,
+        accentColor: VooColors.accentColor,
+        scaffoldBackgroundColor: VooColors.pageBgColor,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: VooColors.primaryColor),
+          textTheme: TextTheme(
+            headline6: TextStyle(fontSize: 18, color: VooColors.titleColor),
+            bodyText2: TextStyle(fontSize: 14, color: VooColors.lightTitleColor),
+            subtitle2: TextStyle(fontSize: 14, color: VooColors.subTitleColor),
+          ),
+          actionsIconTheme: IconThemeData(color: VooColors.primaryColor),
+        ),
         iconTheme: IconThemeData(color: VooColors.primaryColor),
         textTheme: TextTheme(
           headline6: TextStyle(fontSize: 18, color: VooColors.titleColor),
           bodyText2: TextStyle(fontSize: 14, color: VooColors.lightTitleColor),
           subtitle2: TextStyle(fontSize: 14, color: VooColors.subTitleColor),
         ),
-        actionsIconTheme: IconThemeData(color: VooColors.primaryColor),
       ),
-      iconTheme: IconThemeData(color: VooColors.primaryColor),
-      textTheme: TextTheme(
-        headline6: TextStyle(fontSize: 18, color: VooColors.titleColor),
-        bodyText2: TextStyle(fontSize: 14, color: VooColors.lightTitleColor),
-        subtitle2: TextStyle(fontSize: 14, color: VooColors.subTitleColor),
-      ),
-    ),
-    darkTheme: ThemeData(
-      primaryColor: VooColors.pageBgColor,
-      accentColor: VooColors.accentColor,
-      scaffoldBackgroundColor: VooColors.primaryColor,
-      appBarTheme: AppBarTheme(
-        color: VooColors.primaryColor,
-        elevation: 0,
+      darkTheme: ThemeData(
+        primaryColor: VooColors.pageBgColor,
+        accentColor: VooColors.accentColor,
+        scaffoldBackgroundColor: VooColors.primaryColor,
+        appBarTheme: AppBarTheme(
+          color: VooColors.primaryColor,
+          elevation: 0,
+          iconTheme: IconThemeData(color: VooColors.pageBgColor),
+          textTheme: TextTheme(
+            headline6: TextStyle(fontSize: 18, color: VooColors.unWhiteTitleColor),
+            bodyText2: TextStyle(fontSize: 14, color: VooColors.darkTipColor),
+            subtitle2: TextStyle(fontSize: 14, color: VooColors.unWhiteSubTitleColor),
+          ),
+          actionsIconTheme: IconThemeData(color: VooColors.pageBgColor),
+        ),
         iconTheme: IconThemeData(color: VooColors.pageBgColor),
         textTheme: TextTheme(
           headline6: TextStyle(fontSize: 18, color: VooColors.unWhiteTitleColor),
           bodyText2: TextStyle(fontSize: 14, color: VooColors.darkTipColor),
           subtitle2: TextStyle(fontSize: 14, color: VooColors.unWhiteSubTitleColor),
         ),
-        actionsIconTheme: IconThemeData(color: VooColors.pageBgColor),
       ),
-      iconTheme: IconThemeData(color: VooColors.pageBgColor),
-      textTheme: TextTheme(
-        headline6: TextStyle(fontSize: 18, color: VooColors.unWhiteTitleColor),
-        bodyText2: TextStyle(fontSize: 14, color: VooColors.darkTipColor),
-        subtitle2: TextStyle(fontSize: 14, color: VooColors.unWhiteSubTitleColor),
-      ),
-    ),
-    themeMode: ThemeMode.system,
-    routes: routs,
-  ));
+      themeMode: ThemeMode.system,
+      routes: routs,
+      onGenerateRoute: (settings) {
+        print('test settings=${settings.name}');
+        WidgetBuilder builder = routs[settings.name];
+        return MaterialPageRoute(builder: builder);
+      },
+    );
+  }
 }
